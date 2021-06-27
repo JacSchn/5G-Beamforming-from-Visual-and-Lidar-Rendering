b806500ccd85e1be5f3d72cfcdea0c34d570bf1e
Shane Flynn <sflyn248@gmail.com>
2021-06-27 14:27:32 -0500

    updated

--- LiDAR.py
+++ LiDAR.py
@@ -6,6 +6,7 @@ import os
 import signal
 
 buffer = []
+continue_loop = True
 
 class Cloud:
     def __init__(self):
@@ -20,19 +21,22 @@ class Cloud:
 PointCloud = Cloud()
 
 def process_data(buffer, pc):
-    
+
     for point in buffer:
-        if(point[0]==116):
+        if(point[0]=='t'):
             pnt = point.decode("utf-8")
+            if (float(pnt.split()[3]) == 0.0):
+                 continue
             p = [float(pnt.split()[1]), float(pnt.split()[3]), time.time()]
             pc.AddData(p)
     #print(pc.GetData())
-            
+    #print("Done with process_data")
 
 def run_command(command, buffer, pc,timeout):
     count = 0
     process = subprocess.Popen(shlex.split(command), stdout=subprocess.PIPE)
-    while True:
+    LiDARThread = threading.currentThread()
+    while getattr(LiDARThread, "do_run", True):
         output = process.stdout.readline()
         if(timeout):
             count += 1
@@ -43,14 +47,35 @@ def run_command(command, buffer, pc,timeout):
         if output:
             buffer.insert(0, output.strip())
             process_data(buffer, pc)
-    #os.killpg(os.getpgid(process.pid), signal.SIGTERM)
+    print("Out of run_command loop")
+    print("Process PID %s Type: %s" % (process.pid, type(process.pid)))
+    print("OS.getpgid(process.pid) %s" % os.getpgid(process.pid))
+    print("os.getppid() %s" % os.getppid())
+    print("process.pid + 1 %s Type: %s" % ((process.pid + 1), type(process.pid + 1)))
+    #process.send_signal(signal.SIGINT)
+    #os.killpg(os.getpgid(process.pid), signal.CTRL_C_EVENT)
+    stop_LiDAR(process)
+    #process.terminate() #Not permitted. Use 'sudo' when running Program to allow
+    #os.killpg(os.getpgid(process.pid + 1), signal.SIGINT) #kills program
+
+def stop_LiDAR(self):
+        print("Self PID: %s" % self.pid)
+        os.system("sudo kill -2 %s" % str(self.pid + 1))
+        print("Printing ls")
+        os.system("ls")
 
 LiDARThread = threading.Thread(target=run_command, args=('sudo ./ultra_simple /dev/ttyUSB0',buffer,PointCloud,False))
 LiDARThread.start()
 Start = time.time()
-LiDARThread.join(30)
+LiDARThread.join(60)
+if LiDARThread.isAlive():
+    print("I AM STILL ALIVE!!")
+    LiDARThread.do_run = False
+    #LiDARThread.join()
+    if LiDARThread.isAlive():
+        print("NOT DEAD YET")
 print(time.time() - Start)
-#print(PointCloud.GetData())
+# print(PointCloud.GetData())
 
 
 
