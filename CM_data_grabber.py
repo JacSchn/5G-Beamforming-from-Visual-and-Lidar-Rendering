aa7b924e8c313af99d1e1455e8c434ea54d89f52
flynn248 <flynn248@gmail.com>
2021-08-16 11:24:48 -0500

    Potential scripts to collect data from router

--- CM_data_grabber.py
+++ CM_data_grabber.py
@@ -1,13 +1,26 @@
-from pexpect import pxssh
+#!/usr/bin/env python3
 
-s = pxssh.pxssh()
+import redexpect as red
 
-if not s.login('192.168.1.7', 'root'):
-    print("SSH session failed on login")
-    print(str(s))
-else:
-    print("SSH session login successful")
-    s.sendline('ls /')
-    s.prompt()  # match the prompt
-    print(s.before)  # print everything before the prompt
-    s.logout()
+def timer():
+    dog = 2
+
+def save(output_str):
+    cat = 1
+
+def main():
+    username = "root"
+    hostname = "192.168.1.7"
+    pswd = "12345678"
+    module = red.RedExpect(encoding='utf8', expect_timeout=0)
+    print(hostname)
+    module.login(hostname=hostname, username=username, password=None, allow_agent=True, timeout=None)
+    #print(module.command('cat /sys/kernel/debug/ieee80211/phy2/wil6210/bf', remove_newline=True))
+    #print(module.command('cat /sys/kernel/debug/ieee80211/phy2/wil6210/sweep_dump', remove_newline=True))
+    print(module.command('scp root@192.168.100.10:test.txt ./', remove_newline=True))
+    print(module.command('ls', remove_newline=True))
+    module.exit()
+
+
+if __name__=='__main__':
+    main()
---
+++ v1_CM_data_grabber.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from pexpect import pxssh
+
+s = pxssh.pxssh()
+
+if not s.login('192.168.1.7', 'root'):
+    print("SSH session failed on login")
+    print(str(s))
+else:
+    print("SSH session login successful")
+    s.sendline('ls /')
+    s.prompt()  # match the prompt
+    print(s.before)  # print everything before the prompt
+    s.logout()
