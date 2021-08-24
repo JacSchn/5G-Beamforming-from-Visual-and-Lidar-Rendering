d1c5791eacd277928c06dbb015c4bc28ed20f094
JacSchn <79425177+JacSchn@users.noreply.github.com>
2021-08-24 12:41:03 -0500

    Create USB_CAM_Sub.py

---
+++ Cameras/USB_CAM_Sub.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env/python3
+import os
+import rospy
+import csv
+import time
+import numpy as np
+from std_msgs.msg import String
+from rospy_tutorials.msg import Floats
+from rospy.numpy_msg import numpy_msg
+
+
+class FileCount:
+    file_cnt = len(os.listdir('/home/musk/data/USB_CAM'))
+
+    def get_count(self):
+        return self.file_cnt
+
+    def increment(self):
+        self.file_cnt += 1
+
+class CameraTimeStamp:
+    cam_ts = None
+
+    def time(self):
+        return self.cam_ts
+
+    def update(self, new_ts):
+        self.cam_ts = new_ts
+
+'''
+args[0] = FileCount
+args[1] = CameraTimeStamp
+'''
+def callback(data, args):
+    file_path = "/home/musk/data/camera/cam_data_%s" % str(args[0].get_count(args[0]))
+    #data.data = data.data.astype(dtype=np.uint8, copy=False)
+    np.savez(file_path, args[1].time(args[1]),data.data.astype(dtype=np.uint8, copy=False))
+    args[0].increment(args[0])
+    print(args[1].time(args[1]))
+
+def time_callback(data, arg):
+    arg.update(arg, float(data.data))
+
+def listener():
+    rospy.init_node('USBcam_data_sub', anonymous=True)
+    rospy.Subscriber('USBcam_data_time', String, time_callback, callback_args=(CameraTimeStamp))
+    rospy.Subscriber('USBcam_data', numpy_msg(Floats), callback, callback_args=(FileCount,CameraTimeStamp))
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    listener()
