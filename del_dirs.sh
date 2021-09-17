ae68f62f4a7fde6252c893dd2316ce7d1e0e79da
flynn248 <flynn248@gmail.com>
2021-09-17 17:11:07 -0500

    add server and client router

--- del_dirs.sh
+++ del_dirs.sh
@@ -3,8 +3,8 @@
 # Deletes all saved data for all collection methods
 
 # Remove data from files
-rm -r $HOME/data/csi_camera/*
 rm -r $HOME/data/lidar/*
 rm -r $HOME/data/front_usb/*
 rm -r $HOME/data/rear_usb/*
-#rm -r $HOME/data/
+rm -r $HOME/data/server_router/*
+rm -r $HOME/data/client_router/*
