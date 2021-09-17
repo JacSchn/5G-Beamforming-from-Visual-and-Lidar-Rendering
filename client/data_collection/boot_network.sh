00ae69dd9adcb51084ee6c9cdeb3669fa1b730e4
flynn248 <flynn248@gmail.com>
2021-09-17 14:10:26 -0500

    update iperf3 command

--- client/data_collection/boot_network.sh
+++ client/data_collection/boot_network.sh
@@ -8,5 +8,8 @@ if [ $iperf_num -eq 4 ]; then
 	echo "This is iperf3 server"
 else
 # Increase the value of -t for longer ping time
-	iperf3 -c 192.168.100.7 -p 5103 -t 3600 &
+# -i Speed of bandwith information
+# --logfile Logs information about connection
+
+	iperf3 -c 192.168.100.7 -p 5103 -t 3600 -i 60 --logfile network_output
 fi
