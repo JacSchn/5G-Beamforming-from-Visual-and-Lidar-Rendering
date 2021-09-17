11506a86d221ff331afdc433b1ec05094e75b03f
flynn248 <flynn248@gmail.com>
2021-09-17 11:04:28 -0500

    del data

--- del_dirs.sh
+++ del_dirs.sh
@@ -2,15 +2,9 @@
 
 # Deletes all saved data for all collection methods
 
-# Remove directories with data
-rm -r $HOME/data/csi_camera/
-rm -r $HOME/data/lidar/
-rm -r $HOME/data/front_usb/
-rm -r $HOME/data/rear_usb/
+# Remove data from files
+rm -r $HOME/data/csi_camera/*
+rm -r $HOME/data/lidar/*
+rm -r $HOME/data/front_usb/*
+rm -r $HOME/data/rear_usb/*
 #rm -r $HOME/data/
-
-# Create directories
-mkdir $HOME/data/csi_camera
-mkdir $HOME/data/lidar
-mkdir $HOME/data/front_usb/
-mkdir $HOME/data/rear_usb/
