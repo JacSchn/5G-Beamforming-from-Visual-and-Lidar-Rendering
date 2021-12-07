4e3b8400d43aa57a4ce709a92e97613d23271600
Shane Flynn <sflynn248@gmail.com>
2021-12-07 17:55:42 -0600

    add to single class

--- usb_cam/scripts/app_cam_sub.py
+++ usb_cam/scripts/app_cam_sub.py
@@ -19,102 +19,99 @@ import subprocess
 
 import cv2
 
-STATUS = "0"
+class AppCallback:
+    def __init__(self, port_name, time_name, data_dest) -> None:
+        self.status = "0"
+        self.statusSub = rospy.Subscriber('sensor_status', String, statusCallback, callback_args=(port_name, time_name, data_dest))
+        self.timeSub = rospy.Subscriber(time_name, String, time_callback, callback_args=(CameraTimeStamp))
+        self.camSub = rospy.Subscriber(port_name, numpy_msg(Floats), callback, callback_args=(FileCount,CameraTimeStamp, data_dest, usb))
+        
+    def statusCallback(self, status):
+        self.status = status.data
+        print(f'Current status is {self.status}')
 
-def parse_args():
-    # Parse input arguments
-    desc = 'Subscriber for the Sony IMX322 USB camera publisher.\nSaves image arrays to a specified folder as a flattened array.'
-    parser = argparse.ArgumentParser(description=desc)
-    parser.add_argument('--vid', dest='video_dev',
-                        help='device # of USB webcam (/dev/video?) [1]',
-                        default=1, type=int)
+    def parse_args():
+        # Parse input arguments
+        desc = 'Subscriber for the Sony IMX322 USB camera publisher.\nSaves image arrays to a specified folder as a flattened array.'
+        parser = argparse.ArgumentParser(description=desc)
+        parser.add_argument('--vid', dest='video_dev',
+                            help='device # of USB webcam (/dev/video?) [1]',
+                            default=1, type=int)
 
-    parser.add_argument('--dest', dest='data_dest', # ex. "--dest front_usb"
-                        help='destination folder for camera data',
-                        default=None)
+        parser.add_argument('--dest', dest='data_dest', # ex. "--dest front_usb"
+                            help='destination folder for camera data',
+                            default=None)
 
-    args = parser.parse_args()
-    return args
+        args = parser.parse_args()
+        return args
 
-class FileCount:
-    file_path = ""
-    file_cnt = 0
-    init = True
+    class FileCount:
+        file_path = ""
+        file_cnt = 0
+        init = True
 
-    def init_count(self, data_dest):
-        self.file_path = "/home/musk/data/%s" % data_dest
-        self.file_cnt = len(os.listdir(self.file_path))
-        print("Init: %i" % self.file_cnt)
+        def init_count(self, data_dest):
+            self.file_path = "/home/musk/data/%s" % data_dest
+            self.file_cnt = len(os.listdir(self.file_path))
+            print("Init: %i" % self.file_cnt)
 
-    def set_init(self):
-        self.init = False
+        def set_init(self):
+            self.init = False
 
-    def get_init(self):
-        return self.init
+        def get_init(self):
+            return self.init
 
-    def get_count(self):
-        return self.file_cnt
+        def get_count(self):
+            return self.file_cnt
 
-    def increment(self):
-        self.file_cnt += 1
+        def increment(self):
+            self.file_cnt += 1
 
-class CameraTimeStamp:
-    cam_ts = None
+    class CameraTimeStamp:
+        cam_ts = None
 
-    def time(self):
-        return int(self.cam_ts)
+        def time(self):
+            return int(self.cam_ts)
 
-    def update(self, new_ts):
-        self.cam_ts = new_ts
+        def update(self, new_ts):
+            self.cam_ts = new_ts
 
-class Sensor:
-    def __init__(self, stat):
-        self.status = stat
+    class Sensor:
+        def __init__(self, stat):
+            self.status = stat
 
-'''
-args[0] = FileCount
-args[1] = CameraTimeStamp
-args[2] = data_dest
-'''
-def callback(data, args):
-    if args[3].status == "0":
-        return
-    if args[0].get_init(args[0]):
-        print("Initialize count")
-        args[0].init_count(args[0], args[2])
-        args[0].set_init(args[0])
+    '''
+    args[0] = FileCount
+    args[1] = CameraTimeStamp
+    args[2] = data_dest
+    '''
+    def callback(self, data, args):
+        if self.status == "0":
+            return
+        if args[0].get_init(args[0]):
+            print("Initialize count")
+            args[0].init_count(args[0], args[2])
+            args[0].set_init(args[0])
 
-    file_path = "/home/musk/data/%s/usb_data_%s" % (str(args[2]), str(args[0].get_count(args[0])))
-    #data.data = data.data.astype(dtype=np.uint8, copy=False)
-    np.savez(file_path, args[1].time(args[1]),data.data.astype(dtype=np.uint8, copy=False))
-    args[0].increment(args[0])
-    print(args[1].time(args[1]))
+        file_path = "/home/musk/data/%s/usb_data_%s" % (str(args[2]), str(args[0].get_count(args[0])))
+        #data.data = data.data.astype(dtype=np.uint8, copy=False)
+        np.savez(file_path, args[1].time(args[1]),data.data.astype(dtype=np.uint8, copy=False))
+        args[0].increment(args[0])
+        print(args[1].time(args[1]))
 
-def time_callback(data, arg):
-    arg.update(arg, float(data.data))
-
-
-class AppCallback(sensor, args):
-    port_name = args[0]
-    time_name = args[1]
-    data_dest = args[2]
-
-    usb = Sensor(stat="0")
-    usb.status = sensor.data
-    rospy.Subscriber(time_name, String, time_callback, callback_args=(CameraTimeStamp))
-    rospy.Subscriber(port_name, numpy_msg(Floats), callback, callback_args=(FileCount,CameraTimeStamp, data_dest, usb))
-    print(f"In with data = {sensor.data}")
+    def time_callback(data, arg):
+        arg.update(arg, float(data.data))
 
 
 def main():
     args = parse_args()
     port = args.video_dev
     data_dest = args.data_dest
-
     port_name = ("usb_port_%s" % str(port))
     time_name = ("ts_port_%s" % str(port))
+
     rospy.init_node('usb_cam_sub', anonymous=True)
-    rospy.Subscriber('sensor_status', String, appCallback, callback_args=(port_name, time_name, data_dest))
+    usbData = AppCallback(port_name=port_name, time_name=time_name, data_dest=data_dest)
     rospy.spin()
 
 
