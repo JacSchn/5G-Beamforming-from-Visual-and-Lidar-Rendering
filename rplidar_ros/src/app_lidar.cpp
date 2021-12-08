dd8a4cca79f0dd79e2e672cda395017c02b91c3c
Shane Flynn <sflynn248@gmail.com>
2021-12-08 17:03:48 -0600

    add file specifically for web app

---
+++ rplidar_ros/src/app_lidar.cpp
@@ -0,0 +1,125 @@
+/*
+ * Copyright (c) 2014, RoboPeak
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without 
+ * modification, are permitted provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, 
+ *    this list of conditions and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, 
+ *    this list of conditions and the following disclaimer in the documentation 
+ *    and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, 
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR 
+ * PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
+ * CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, 
+ * EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, 
+ * PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; 
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+ * WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR 
+ * OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, 
+ * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+/*
+ *  RoboPeak LIDAR System
+ *  RPlidar ROS Node client test app
+ *
+ *  Copyright 2009 - 2014 RoboPeak Team
+ *  http://www.robopeak.com
+ * 
+ */
+//Code modified by Shane Flynn
+
+#include <chrono> // for timestamp
+#include <iostream>
+#include <fstream>
+#include "ros/ros.h"
+#include "sensor_msgs/LaserScan.h"
+#include "web_app_companion/Sensor.h"
+
+// C library files
+#include <stdio.h>
+#include <sys/types.h>
+#include <dirent.h>
+
+#include <typeinfo>
+#define RAD2DEG(x) ((x)*180./M_PI)
+
+// Manages lidar file names
+class LidarProcessor
+{
+    int numFiles = -2; //avoids counting two hidden dir
+    std::string filePath = "/home/musk/data/lidar/scan_data_";
+
+    public:
+        LidarProcessor();
+        void updateFilePath();
+        void scanCallback(const sensor_msgs::LaserScan::ConstPtr&);
+};
+
+LidarProcessor::LidarProcessor(){
+//  count number of files in lidar directory
+    DIR *dp;
+    struct dirent *ep;
+    dp = opendir("/home/musk/data/lidar/");
+
+    if(dp != NULL)
+    {
+        while(ep = readdir(dp))
+            numFiles++;
+        (void)closedir(dp);
+    }
+    else
+        perror("Couldn't open the directory");
+
+
+    filePath += std::to_string(numFiles) + ".txt"; //initialize first file
+}
+
+// Update the file name for new scan
+void LidarProcessor::updateFilePath(){
+    numFiles++;
+    filePath = "/home/musk/data/lidar/scan_data_" + std::to_string(numFiles) + ".txt";
+}
+
+void LidarProcessor::scanCallback(const sensor_msgs::LaserScan::ConstPtr& scan){
+    //auto lidarOutputFile = std::fstream("~/data/lidar/scan_data.txt",std::ios::out);
+    std::ofstream lidarOutputFile;
+    lidarOutputFile.open(filePath, std::ios::out | std::ios::app);
+    updateFilePath();
+
+    int count = scan->scan_time / scan->time_increment; //num of data points in scan
+    //ROS_INFO("I heard a laser scan %s[%d]:", scan->header.frame_id.c_str(), count);
+
+    //timestamp
+    std::chrono::milliseconds ms = std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::system_clock::now().time_since_epoch());
+
+    //ROS_INFO("angle_range, %f, %f", RAD2DEG(scan->angle_min), RAD2DEG(scan->angle_max));
+
+    lidarOutputFile << ms.count() << "\n"; //Output timestamp to file
+    std::cout << "Time: " << ms.count() << std::endl; //Display timestamp to console
+
+    for(int i = 0; i < count; i++) { //Convert scan data and output to file
+        float degree = RAD2DEG(scan->angle_min + scan->angle_increment * i);
+        lidarOutputFile << degree << " " << scan->ranges[i] << "\n";
+    }
+
+    lidarOutputFile.close();
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "rplidar_node_write_file");
+    ros::NodeHandle n;
+    LidarProcessor lidarProcessor;
+    ros::Subscriber sub = n.subscribe<sensor_msgs::LaserScan>("/scan", 1000, &LidarProcessor::scanCallback, &lidarProcessor);
+
+    ros::spin();
+
+    return 0;
+}
