bbbc84fa1b0d75f582bf89a0c8b24bb7939e0189
Shane Flynn <sflynn248@gmail.com>
2021-12-08 11:33:54 -0600

    send json data to web app

--- web_app_companion/scripts/app_companion.py
+++ web_app_companion/scripts/app_companion.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from typing import List
 import requests as req
 import time
 import rospy
@@ -8,41 +9,70 @@ class Sensor:
     def __init__(self, name: str) -> None:
         self.name = name
         self.statusOn = False
+        self.timeTurnedOn = 0
+        self.timeTurnedOff = time.time()
 
+def initJsonData(sensors: list) -> dict:
+    retDict = {
+        "sensor": []
+    }
 
-def postSensorData(URL: str, sensor: Sensor) -> bool:
+    if len(sensors) == 0:
+        retDict['sensor'].append({
+            "name": '',
+            "status": False,
+            "turnedOnAt": None,
+            "turnedOffAt": None
+        })
+        return retDict
+
+    for sensor in sensors:
+        retDict['sensor'].append({
+            "name": sensor.name,
+            "status": sensor.status,
+            "turnedOnAt": sensor.timeTurnedOn,
+            "turnedOffAt": sensor.timeTurnedOff
+        })
+
+    
+    return retDict
+
+def postSensorData(URL: str, sensors: list) -> bool:
     '''
     Send http header data of a sensor to app with url=URL and endpoint being /micro
     '''
     endpoint = "micro"
     URL.join(endpoint)
-    data = {
-        "sensor": sensor.name,
-        "timestamp": sensor.timestamp
-    }
+    data = initJsonData(sensors=sensors)
 
-    ret_val = req.post(url=URL, headers=data).text
+    ret_val = req.post(url=URL, json=data).txt
     if ret_val == 200:
-        print(f"Post request SUCCESS for {sensor.name}")
+        print(f"Post request SUCCESS")
         return True
     elif ret_val == 500:
-        print(f"Post request FAILURE for {sensor.name}")
+        print(f"Post request FAILURE")
         return False
 
 
-def getState(URL: str) -> bool:
+def getState(URL: str, sensors: list) -> bool:
     '''
     Get current state at endpoint /getState
     '''
     endpoint = "getState"
     URL.join(endpoint)
-    state = req.get(url=URL)
-    print(f'My current state is {state.text}')
-    return state.text
+    data = initHeaderData(sensors=sensors)
 
-def pubInitState(sensors: list, pub) -> None:
+    sensorStates = req.get(url=URL, headers=data).json()
+    
+    for senName, senStat in sensorStates['name'], sensorStates['status']:
+        print(f'State of {senName} is {senStat}.')
+
+    
+    return 
+
+def pubCurrState(sensors: list, pub) -> None:
     '''
-    Publish to all sensors their initial state.
+    Publish to all sensors their current state.
     '''
     pub.publish("0")
     # for sensor in sensors:
@@ -70,7 +100,7 @@ def runApp():
         if rospy.is_shutdown():
             print("Terminating Web App Companion")
             break
-        pubInitState(front_usb, pub=pub)
+        pubCurrState(front_usb, pub=pub)
         time.sleep(2)
 
         
